d96880ec42418578706825d36c85beac3e206740
Heidiki <abdelattie@gmail.com>
2023-03-28 12:39:36 +0000

    add movies controller

---
+++ backend/api/movies.controller.js
@@ -0,0 +1,30 @@
+import moviesDAO from "../dao/moviesDAO.js";
+
+export default class MoviesController {
+    static async apiGetMovies(req, res, next) {
+        const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage, 10) : 20;
+        const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+
+        let filters = {};
+        if (req.query.genre) {
+            filters.genre = req.query.genre;
+        } else if (req.query.title) {
+            filters.title = req.query.title;
+        }
+
+        const { moviesList, totalNumMovies } = await moviesDAO.getMovies({
+            filters,
+            page,
+            moviesPerPage,
+        });
+
+        let response = {
+            movies: moviesList,
+            page: page,
+            filters: filters,
+            entries_per_page: moviesPerPage,
+            total_results: totalNumMovies,
+        };
+        res.json(response);
+    }
+}
